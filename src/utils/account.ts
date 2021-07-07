d53769a179b4e16c7fd8f537926ca3ddd8ebafce
iq9891 <649076408@qq.com>
2021-07-07 18:33:49 +0800

    fix($logout): error

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -8,7 +8,7 @@ import { getEnvConfig } from '../env';
 
 const goLoginPage = (isRedirect?: boolean) => {
   const { VITE_COMMON } = getEnvConfig();
-  const redirectHost = VITE_COMMON || 'http://localhost: 3000/';
+  const redirectHost = VITE_COMMON || 'http://localhost:3000/';
   const redirectUrl = isRedirect ? `?redirect=${window.location.href}` : '';
   siteReplace(`${redirectHost}login${redirectUrl}`);
 };
