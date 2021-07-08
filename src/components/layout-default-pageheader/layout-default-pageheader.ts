78e88f95f41402acdd9038e29a517aabb2d64bba
iq9891 <649076408@qq.com>
2021-07-08 18:38:55 +0800

    fix($default): update nav

--- src/components/layout-default-pageheader/layout-default-pageheader.ts
+++ src/components/layout-default-pageheader/layout-default-pageheader.ts
@@ -1,61 +1,32 @@
 import { defineComponent, watch, ref } from 'vue';
-import { useStore } from 'vuex';
 import { ArrowLeftOutlined } from '@ant-design/icons-vue';
 import { useRouter } from 'vue-router';
 
-import { getAvtiveKey } from '../../utils/menus';
-import routers from './use-path';
-import { isEmpty } from 'lodash-es';
+import {
+  getBreadcrumbRoutes,
+  getTitle,
+  getHeaderGoBack,
+} from './use-path';
 
 export default defineComponent({
   components: {
     ArrowLeftOutlined,
   },
   setup() {
-    const [routes] = routers();
     const { currentRoute } = useRouter();
-    const breadcrumbRoutes = ref<any>([]);
-    const myStores = useStore();
-    const title = ref('');
-    watch([() => myStores.state.external.navs, () => currentRoute.value.fullPath, () => myStores.state.external.navTitle], ([newNavs, newFullPath, newNavTitle]) => {
-      const {
-        twoNavItem,
-        threeNavItem,
-      } = getAvtiveKey(newNavs, newFullPath);
-      // 切换导航的时候重置
-      breadcrumbRoutes.value = [];
-
-      if (newNavTitle) {
-        breadcrumbRoutes.value.push({
-          path: '/',
-          title: newNavTitle,
-        });
-      }
-
-      if (!isEmpty(twoNavItem)) {
-        breadcrumbRoutes.value.push({
-          path: twoNavItem.path,
-          title: twoNavItem.name,
-        });
-      }
-      if (!isEmpty(threeNavItem)) {
-        breadcrumbRoutes.value.push({
-          path: threeNavItem.path,
-          title: threeNavItem.name,
-        });
-      }
-      if (routes.value.length > 0 && threeNavItem.name !== routes.value[0].meta.title) {
-        breadcrumbRoutes.value.push({
-          path: routes.value[0].path,
-          title: routes.value[0].meta.title,
-        });
-      }
-      title.value = breadcrumbRoutes.value[breadcrumbRoutes.value.length - 1].title;
+    const headerGoBack = ref(getHeaderGoBack(currentRoute.value));
+    const title = ref(getTitle(currentRoute.value));
+    const breadcrumbRoutes = ref<any[]>(getBreadcrumbRoutes(currentRoute.value));
+    watch([() => currentRoute.value], ([newRoute]) => {
+      title.value = getTitle(newRoute);
+      breadcrumbRoutes.value = getBreadcrumbRoutes(newRoute);
+      headerGoBack.value = getHeaderGoBack(newRoute);
     });
 
     return {
       breadcrumbRoutes,
       title,
+      headerGoBack,
     };
   },
 });
--- src/components/layout-inner-pageheader/layout-inner-pageheader.ts
+++ src/components/layout-inner-pageheader/layout-inner-pageheader.ts
@@ -1,40 +1,12 @@
 import { defineComponent, watch, ref } from 'vue';
-import type { RouteLocationNormalizedLoaded } from 'vue-router';
 import { ArrowLeftOutlined } from '@ant-design/icons-vue';
 import { useRouter } from 'vue-router';
-import { hasOwn } from '@vue/shared';
-import { isFunction } from 'lodash-es';
 
-const getRouterMeta = (currentRoute: RouteLocationNormalizedLoaded, key: string) => {
-  if (!key) {
-    return '';
-  }
-  if (
-    hasOwn(currentRoute, 'meta')
-    && hasOwn(currentRoute.meta, key)
-    && (currentRoute.meta as any)[key].length
-  ) {
-    return (currentRoute.meta as any)[key];
-  }
-  return '';
-};
-const getBreadcrumbRoutes = (currentRoute: RouteLocationNormalizedLoaded) => {
-  return getRouterMeta(currentRoute, 'breadcrumbRoutes');
-};
-const getTitle = (currentRoute: RouteLocationNormalizedLoaded) => {
-  return getRouterMeta(currentRoute, 'title');
-};
-
-const getHeaderGoBack = (currentRoute: RouteLocationNormalizedLoaded) => {
-  if (
-    hasOwn(currentRoute, 'meta')
-    && hasOwn(currentRoute.meta, 'headerGoBack')
-    && isFunction((currentRoute.meta as any).headerGoBack)
-  ) {
-    return (currentRoute.meta as any).headerGoBack;
-  }
-  return () => {};
-};
+import {
+  getBreadcrumbRoutes,
+  getTitle,
+  getHeaderGoBack,
+} from '../layout-default-pageheader/use-path';
 
 export default defineComponent({
   components: {
