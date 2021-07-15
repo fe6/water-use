93612edbe68b501fd2e24ef57cc58c3934dda0b9
iq9891 <649076408@qq.com>
2021-07-15 17:35:41 +0800

    trivia($token): update key name

--- src/constant/base.ts
+++ src/constant/base.ts
@@ -1,11 +1,11 @@
-export const EVENT_TOKE = 'Em-Consumer-Token';
+export const EVENT_TOKE = 'Em-Business-Token';
 
 export const EVENT_TOKEN_PREFIX = 'Bearer ';
 
-export const EVENT_PROFILE = 'Em-Consumer-Profile';
+export const EVENT_PROFILE = 'Em-Business-Profile';
 
-export const EVENT_PHONE = 'Em-Consumer-Phone';
+export const EVENT_PHONE = 'Em-Business-Phone';
 
-export const EVENT_SHOP_ID = 'X-Request-Em-Consumer-Shop-Id';
+export const EVENT_SHOP_ID = 'X-Request-Em-Business-Shop-Id';
 
-export const EVENT_SHOP_INFO = 'X-Request-Em-Consumer-Shop-Info';
+export const EVENT_SHOP_INFO = 'X-Request-Em-Business-Shop-Info';
